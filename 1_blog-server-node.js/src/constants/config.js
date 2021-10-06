8091a58245034611da0ab0088ca03ecc096e8b14
HouJian <hj1254123@qq.com>
2021-10-06 22:43:28 +0800

    登录接口开发完毕 使用 token 方式鉴权

--- 1_blog-server-node.js/src/constants/config.js
+++ 1_blog-server-node.js/src/constants/config.js
@@ -1,7 +1,17 @@
+const fs = require("fs")
+const path = require("path")
+const PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, './private.key'))
+const PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './public.key'))
+
+
+
 const MD5_SALT = 'jwfkllskd';
 const REGISTRY_INVITATION_CODE = '7355608'
 
+
 module.exports = {
   MD5_SALT,
-  REGISTRY_INVITATION_CODE
+  REGISTRY_INVITATION_CODE,
+  PRIVATE_KEY,
+  PUBLIC_KEY
 }
--- 1_blog-server-node.js/src/routes/auth.js
+++ 1_blog-server-node.js/src/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 
 const userController = require('../model/user')
+
+const { PRIVATE_KEY, PUBLIC_KEY } = require('../constants/config')
 
 router.post('/register', (req, res, next) => {
   // // - 拿到用户数据
@@ -27,14 +30,36 @@ router.post('/login', (req, res, next) => {
     .then(data => {
       if(data.message === '登录成功') {
         // 颁发token
-        
+        const token = jwt.sign(data.data, PRIVATE_KEY, {
+          expiresIn: 86400, //1天过期
+          algorithm: 'RS256'// 设置算法为 RS256
+        })
+        data.data.token = token
+        res.json(data)
+      } else {
+        // 登录失败
+        res.json(data)
       }
-      // 向客户端返回结果
-      res.json(data)
     })
     .catch(err => {
       next(err)
     })
 })
 
+// 鉴权测试
+router.get('/test', (req, res) => {
+  let token = ''
+  const authorization = req.headers.authorization
+  if(authorization) {
+    token = authorization.replace('Bearer ', '')
+  }
+  try {
+    // 验证签名，需要使用公钥
+    const result = jwt.verify(token, PUBLIC_KEY)
+    res.json(`欢迎回来${result.userName}`)
+  } catch(error) {
+    res.json(`鉴权失败，请重新登录：${error.message}`)
+  }
+
+})
 module.exports = router
