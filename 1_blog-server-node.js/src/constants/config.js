76e06f00482694bca7ffffce79d1a9304f373965
HouJian <hj1254123@qq.com>
2021-10-07 17:31:05 +0800

    密钥部分代码添加错误处理

--- 1_blog-server-node.js/src/constants/config.js
+++ 1_blog-server-node.js/src/constants/config.js
@@ -1,10 +1,18 @@
 const fs = require("fs")
 const path = require("path")
-const PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, './private.key'))
-const PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './public.key'))
 
+let PRIVATE_KEY
+let PUBLIC_KEY
 
+// TODO:项目上线时重新生成
+try {
+  PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, './private.key'))
+  PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './public.key'))
+} catch(error) {
+  console.log('密钥读取出错：', error.message);
+}
 
+// TODO:项目上线时修改
 const MD5_SALT = 'jwfkllskd';
 const REGISTRY_INVITATION_CODE = '7355608'
 
