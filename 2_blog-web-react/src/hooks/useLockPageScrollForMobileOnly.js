77eeef12254e5edb0c9f21e1823eb7085d6d5f6e
HouJian <hj1254123@qq.com>
2022-03-07 21:37:36 +0800

    移动端锁定页面滚动后，调整窗口到大屏还是被锁定的 bug 修复

--- 2_blog-web-react/src/hooks/useLockPageScrollForMobileOnly.js
+++ 2_blog-web-react/src/hooks/useLockPageScrollForMobileOnly.js
@@ -1,15 +1,26 @@
 import { useCallback, useEffect } from "react";
+import { useMedia } from 'react-use'
 
 export default function useLockPageScrollForMobileOnly(lock) {
+  const isMaxWidth1240px = useMedia('(max-width: 1240px)')
+
   const fn = useCallback(e => {
     e.preventDefault()
   }, [])
 
   useEffect(() => {
     if(lock) {
+      // 大屏特殊处理
+      if(!isMaxWidth1240px) {
+        document.body.removeEventListener('touchmove', fn)
+        return
+      }
+      // 阻止滚动
       document.body.addEventListener('touchmove', fn, { passive: false });
     } else {
+      // 恢复滚动
       document.body.removeEventListener('touchmove', fn);
     }
-  }, [lock])
+
+  }, [lock, isMaxWidth1240px])
 }
