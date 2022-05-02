79fd55e3424f970d59856ec4fab52ab541c5868c
HouJian <hj1254123@qq.com>
2022-05-02 13:34:06 +0800

    功能：完成标签页开发

--- 2_blog-web-react/src/pages/TagsPage/index.js
+++ 2_blog-web-react/src/pages/TagsPage/index.js
@@ -4,57 +4,73 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 import { useSetHeaderTitle } from '../../hooks/useSetHeaderTitle'
 
-import { Header } from '../../components'
+import { ArticleCardList, Header } from '../../components'
 import { TagsWrapper, Main, TagNameNavWrapper } from './style'
 import useSWRImmutable from 'swr'
 import hjRequest from '../../services/request'
 import classNames from 'classnames'
 
 const TagsPage = memo(() => {
-  const [isUnfold, setIsUnfold] = useState(false)
   useSetHeaderTitle("Tags")
-  const navigate = useNavigate()
+  const [isUnfold, setIsUnfold] = useState(false) //控制展开tagNav
 
+  const navigate = useNavigate()
   const { tagName = '全部' } = useParams()
 
   const { data } = useSWRImmutable(`/tags/page`, (url) => {
     return hjRequest.get(url).then(d => d)
   })
+
   const titleNameArr = data ? data[0] : []
   const tagMapArticleObj = data ? data[1] : {}
 
-  const tagnameListClass = classNames('tagname-list', {
-    'unfold': isUnfold
-  })
+  function getTagNameNavJSX() {
+    const tagnameListClass = classNames('tagname-list', {
+      'unfold': isUnfold
+    })
+
+    return <TagNameNavWrapper>
+      {/* tag标题导航栏 */}
+      <ul className={tagnameListClass}>
+        <li
+          className={classNames({ 'active': tagName === '全部' })}
+          onClick={() => navigate('/tags')}
+        >
+          全部</li>
+        {
+          titleNameArr.map(item => {
+            return <li
+              key={item.id}
+              className={classNames({ 'active': tagName === item.tagName })}
+              onClick={() => navigate('/tags/page/' + item.tagName)}
+            >{item.tagName}</li>
+          })
+        }
+        {/* 展开按钮 */}
+        <button
+          className='iconfont icon-icon3'
+          onClick={() => setIsUnfold(!isUnfold)}
+        ></button>
+      </ul>
+    </TagNameNavWrapper>
+  }
+
+  function getMainJSX() {
+    if(tagName === '全部') {
+      return titleNameArr.map(item => {
+        let tagName = item.tagName
+        let articlesData = tagMapArticleObj[tagName]
+        return <ArticleCardList key={item.id} title={tagName} articlesData={articlesData} />
+      })
+    } else {
+      return <ArticleCardList title={tagName} articlesData={tagMapArticleObj[tagName]} />
+
+    }
+  }
+
   return (
-
     <TagsWrapper>
-      <Header>
-        <TagNameNavWrapper>
-          <ul className={tagnameListClass}>
-            <li
-              className={tagName === '全部' ? 'active' : undefined}
-              onClick={() => navigate('/tags')}
-            >
-              全部</li>
-            {
-              titleNameArr.map(item => {
-                const isActive = tagName === item.tagName ? 'active' : undefined
-                return <li
-                  key={item.id}
-                  className={isActive}
-                  onClick={() => navigate('/tags/page/' + item.tagName)}
-                >{item.tagName}</li>
-              })
-            }
-            <button
-              className='iconfont icon-icon3'
-              onClick={() => setIsUnfold(!isUnfold)}
-            ></button>
-          </ul>
-        </TagNameNavWrapper>
-      </Header>
-
+      <Header>{getTagNameNavJSX()}</Header>
       {
         data && <SwitchTransition mode='out-in'>
           <CSSTransition
@@ -63,10 +79,7 @@ const TagsPage = memo(() => {
             key={tagName}
             appear
           >
-
-            <Main>
-
-            </Main>
+            <Main>{getMainJSX()}</Main>
           </CSSTransition>
         </SwitchTransition>
       }
