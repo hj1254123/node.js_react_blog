2805c46f33dee1bf051b2b890f31874ee0ab8ea2
HouJian <hj1254123@qq.com>
2022-05-15 08:55:19 +0800

    功能：markdown样式开发

--- 2_blog-web-react/src/pages/ArticlePage/index.js
+++ 2_blog-web-react/src/pages/ArticlePage/index.js
@@ -2,15 +2,16 @@ import React, { memo, useEffect } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { useParams } from 'react-router-dom'
 import useSWRImmutable from 'swr'
-import Markdown from 'markdown-to-jsx';
-import hljs from 'highlight.js';
+import Markdown from 'markdown-to-jsx'; // markdown解析引擎
+import hljs from 'highlight.js'; // 语法高亮支持
 import 'highlight.js/styles/github.css';
 
 import hjRequest from '../../services/request'
 import { useTitleContext } from '../../context/Title-context'
+import { formatDate } from '../../utils/my-utils';
 
 import { Header } from '../../components'
-import { ArticleWrapper, Main } from './style'
+import { ArticleWrapper, Main, TOC } from './style'
 
 // 本项目用 highlight 会自动给代码块添加对应语言的 class，
 // 这里重写 code，取消markdown-to-jsx添加的无用class属性。
@@ -20,9 +21,6 @@ const ArticlePage = memo(() => {
   useEffect(() => {
     document.documentElement.scrollTop = 0
   }, [])
-  useEffect(() => {
-    hljs.highlightAll()
-  })
 
   const { setTitle } = useTitleContext()
   const { id } = useParams()
@@ -33,7 +31,10 @@ const ArticlePage = memo(() => {
       setTitle(data.data.title)
     }
   })
-  console.log(data)
+  useEffect(() => {
+    hljs.highlightAll()
+  }, [data])
+
   return (
     <ArticleWrapper>
       {
@@ -46,17 +47,36 @@ const ArticlePage = memo(() => {
             appear
           >
             <Main>
-              <Markdown
-                children={data.data.content}
-                options={{
-                  wrapper: 'article', //改变包装元素
-                  slugify: str => str, //标题元素id='中文'正常处理
-                  disableParsingRawHTML: true, //转义尖括号<>
-                  overrides: { // 通过重写code，去掉markdown-to-jsx添加的语言class，交给highlight.js自动添加
-                    code: MyCode
-                  },
-                }}
-              />
+              <article className='markdown-body'>
+                <h1>{data.data.title}</h1>
+                <time>{formatDate(data.data.time)}</time>
+                <Markdown
+                  children={data.data.content}
+                  options={{
+                    forceWrapper: true, //即使只有一个子元素，也包装（本例用div包装）
+                    slugify: str => str, //标题元素id='中文'正常处理
+                    disableParsingRawHTML: true, //转义尖括号<>
+                    overrides: { // 通过重写code，去掉markdown-to-jsx添加的语言class，交给highlight.js自动添加
+                      code: MyCode
+                    },
+                  }}
+                />
+              </article>
+              <TOC>
+                <ul>
+                  <li>1</li>
+                  <li>2
+                    <ul>
+                      <li>1</li>
+                      <li>2</li>
+                      <li>3</li>
+                      <li>4</li>
+                    </ul>
+                  </li>
+                  <li>3</li>
+                </ul>
+              </TOC>
+
             </Main>
           </CSSTransition>
         </>
--- 2_blog-web-react/src/pages/ArticlePage/style.js
+++ 2_blog-web-react/src/pages/ArticlePage/style.js
@@ -1,16 +1,70 @@
 import styled from 'styled-components';
+import 'github-markdown-css/github-markdown-light.css'
 
 export const ArticleWrapper = styled.div`
+  width: 100%;
   min-height: calc(100vh);
-
 `
+
 export const Main = styled.div`
-  width: 960px;
+  display: flex;
+  width: 100%;
   margin: 0 auto;
-  padding: 30px 0 40px;
+  padding: 45px;
+
   @media screen and (max-width: 1040px) {
-    width: 100%;
-    padding: 20px 16px
+    padding: 15px
+  }
+  /* 覆盖一些默认样式 */
+  .markdown-body {
+    flex: 1;
+    position: relative;
+    margin-top: -150px;
+    padding: 35px;
+    border-radius: 4px;
+    min-height: 300px;
+    h1 {
+      border-bottom: none;
+    }
+    time {
+      display: block;
+      margin-top: 8px;
+      font-size: 13px;
+      font-weight: bold;
+      color: #727272;
+    }
+    h2, h3, h4, h5, h6 {
+      color: #3f51b5;
+      ::before {
+        content: '#';
+        color: #c5cae9;
+        padding-right: 5px;
+        margin-left: -16px;
+        visibility: hidden;
+      }
+    }
+    h2:hover, h3:hover, h4:hover, h5:hover, h6:hover {
+      ::before {
+        visibility: visible;
+      }
+    }
+    code, a {
+      color: #ff4081;
+    }
+    pre code {
+      color: inherit;
+    }
+  }
+`
+
+export const TOC = styled.aside`
+  width: 180px;
+  min-height: 100px;
+  @media screen and (max-width: 1040px) {
+    display: none;
+  }
+  > ul {
+    position: fixed;
   }
 `
 
