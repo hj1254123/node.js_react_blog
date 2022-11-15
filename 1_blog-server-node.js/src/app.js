2e75700eac747af6927dfb9da98b33ed0a514e8e
HouJian <hj1254123@qq.com>
2022-11-15 23:06:30 +0800

    chore: 请求封装中请求路径修改

--- 1_blog-server-node.js/src/app.js
+++ 1_blog-server-node.js/src/app.js
@@ -13,11 +13,11 @@ const commentRouter = require('./routes/comment.js');
 const archiveRouter = require('./routes/archive.js')
 
 const app = express()
-const port = 3001
+const port = 3003
 
 // 允许信任url跨域
 app.use((req, res, next) => {
-  const trustList = ['http://localhost:3000', 'http://localhost:3002']
+  const trustList = ['http://localhost:3000', 'http://localhost:3001']
   const origin = req.headers.origin
   if(trustList.indexOf(origin) > -1) {
     res.header('Access-Control-Allow-Origin', origin)
--- 3_blog-admin/src/services/request/config.js
+++ 3_blog-admin/src/services/request/config.js
@@ -5,9 +5,8 @@ const TIMEOUT = 10000;
 // npm test 它总是等于 'test'
 // npm run build 它总是等于 'production' 
 if(process.env.NODE_ENV === 'development') {
-  BASE_URL = 'http://192.168.3.42:3001'
+  BASE_URL = 'http://localhost:3003'
 } else {
-  // BASE_URL = 'http://192.168.3.42:3001'
   BASE_URL = 'http://150.158.168.128:3001'
 }
 
