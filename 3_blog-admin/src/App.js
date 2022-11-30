bf24ae220a5bd991650d89a1f8f1350e394ecce4
HouJian <hj1254123@qq.com>
2022-12-01 00:26:12 +0800

    feat:博客管理系统布局骨架搭建完毕

--- 3_blog-admin/src/App.js
+++ 3_blog-admin/src/App.js
@@ -1,13 +1,90 @@
-import { useRoutes  } from 'react-router-dom'
-import router from './router'
+import logo from './assets/logo.png'
 
-function App() {
-  const element = useRoutes(router)
+import React, { useState } from 'react';
+import {
+  FileTextOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  MessageOutlined,
+  TagsOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <div className="App">
-      {element}
-    </div>
-  )
-}
-
-export default App
+    <Layout style={{
+      minHeight: '100vh',
+    }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div style={{
+          backgroundColor: '#fff',
+          width: '100%',
+          textAlign: 'center'
+        }}>
+          <img src={logo} alt="HouJI's博客管理系统" style={{
+            width: '70%',
+          }} />
+        </div>
+        <Menu
+          style={{
+            minHeight: '100vh'
+          }}
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '/home',
+              icon: <UserOutlined />,
+              label: '首页',
+            },
+            {
+              key: '/articles',
+              icon: <FileTextOutlined />,
+              label: '文章管理',
+            },
+            {
+              key: '/tags',
+              icon: <TagsOutlined />,
+              label: '标签管理',
+            },
+            {
+              key: '/comments',
+              icon: <MessageOutlined />,
+              label: '评论管理',
+            }
+          ]}
+          onClick={({ key }) => {
+            console.log(key)
+          }}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: '0 15px',
+            backgroundColor: '#fff',
+          }}
+        >
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
