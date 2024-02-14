8f8550b7a04c59d84aea955df1348a7347bac3f0
houji <hj1254123@gmail.com>
2024-02-14 16:26:25 +0800

    refactor: 恢复 BASE_URL

--- 2_blog-web-react/src/services/request/config.js
+++ 2_blog-web-react/src/services/request/config.js
@@ -5,8 +5,8 @@ const TIMEOUT = 10000;
 // npm test 它总是等于 'test'
 // npm run build 它总是等于 'production' 
 if(process.env.NODE_ENV === 'development') {
-  // BASE_URL = 'http://localhost:3003'
-  BASE_URL = 'http://192.168.1.2:3003'
+  BASE_URL = 'http://localhost:3003'
+  // BASE_URL = 'http://192.168.1.2:3003'
 } else {
   BASE_URL = 'https://47.108.195.109'
 }
